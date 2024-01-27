48de1380b245cb5fd90f625fd434b4986f2ca59a
saidevops32 <devopsengsai@gmail.com>
2024-01-28 01:06:05 +0530

    roboshop shell script

---
+++ mongodb.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "script started excuting $TIMESTAMP" &>> LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+         echo -e "$2 is .. $R failed $N"
+         exit 1
+    else
+         echo -e "$2 iS.. $G success $N"  
+    fi  
+}
+
+if [ $ID -ne 0 ]
+then
+     echo -e "error : you are not a root user $R become root user $N"
+     exit 1
+else
+     echo "you are a root user"
+fi  
+
+cp mongodb.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "COPIED MONGODB REPO"
