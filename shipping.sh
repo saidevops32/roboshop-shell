bc956cb51bdb3de55b6d2e17dda0941e9a35a2d0
saidevops32 <devopsengsai@gmail.com>
2024-01-28 09:24:08 +0530

    roboshop shell script

--- shipping.sh
+++ shipping.sh
@@ -84,7 +84,7 @@ dnf install mysql -y &>> $LOGFILE
 
 VALIDATE $? "install MySQL client"
 
-mysql -h mysql.daws76s.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+mysql -h mysql.saidev.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
 
 VALIDATE $? "loading shipping data"
 
